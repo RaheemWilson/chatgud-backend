5aa2e9ebdf10615b3103b5f4defd288c18536182
RaheemWilson <wilsonzackery16@gmail.com>
2023-03-28 15:11:11 -0500

    made type changes

--- app/api/category.py
+++ app/api/category.py
@@ -1,10 +1,10 @@
 from typing import List
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 
 from app.schemas.category import Category
 from db import db
 router = APIRouter()
 
-@router.get("/categories", response_model=list[Category], status_code=200)
+@router.get("/categories", response_model=List[Category], status_code=200)
 async def get_category():
     return await db.category.find_many()
--- app/api/proficiency.py
+++ app/api/proficiency.py
@@ -5,6 +5,6 @@ from app.schemas.proficiency import Proficiency
 from db import db
 router = APIRouter()
 
-@router.get("/proficiency", response_model=list[Proficiency], status_code=200)
+@router.get("/proficiency", response_model=List[Proficiency], status_code=200)
 async def get_proficiency():
     return await db.proficiency.find_many()
