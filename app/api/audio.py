5299545990942f48955d86ae527977eee26b53e7
RaheemWilson <wilsonzackery16@gmail.com>
2023-05-11 20:46:36 -0500

    added updated content

--- app/api/audio.py
+++ app/api/audio.py
@@ -1,21 +1,44 @@
 from typing import Optional
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, UploadFile, Form
 from pydantic import BaseModel
-from db import db
+import os
+from urllib.request import urlopen, urlretrieve
+import librosa
+import requests
+import ssl
+import pickle
 
 router = APIRouter()
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 class Translation(BaseModel):
     translation: str
     confidence: Optional[float]
 
-@router.post("/translate", response_model=Translation, status_code=200)
-def translate_audio(sound: UploadFile):
-    return Translation(translation=sound.filename)
-
 @router.post("/evaluate", response_model=Translation, status_code=200)
-def evaluate_audio(file: UploadFile = File(...)):
-    # file_location = f"files/{file.filename}"
+async def evaluate_audio(ref_url: str = Form(...), file: UploadFile = File(...)):
+    audio_name = ref_url.split("/")[-1]
+    audio_path = f"files/audio/{audio_name}"
+    urlretrieve(ref_url, audio_path)
+    
+    file_path = f"files/audio/{file.filename}"
+    with open(file_path, "wb") as f:
+        f.write(await file.read())
+        
+    ref_audio, sr = librosa.load(audio_path)
+    test_audio, sr_file = librosa.load(file_path)
+    
+    os.remove(audio_path)
+    os.remove(file_path)
+    model = None
+    with open("model.pkl", 'rb') as f:
+        print("found")
+        # model = pickle.load(f)
+        
+    return Translation(translation=file.filename)
+
+
+# file_location = f"files/{file.filename}"
     # with open(file_location, "wb+") as file_object:
     #     file_object.write(file.file.read())
-    return Translation(translation=file.filename)
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
 
-# Set all CORS enabled origins
+# Set all CORS enabled originsss
 if settings.BACKEND_CORS_ORIGINS:
     app.add_middleware(
         CORSMiddleware,
